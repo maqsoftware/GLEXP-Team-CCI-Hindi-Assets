2a4d31f5630905c919773fafc8e0e4984cba5f26
Shailin Shah <shailins@maqsoftware.com>
2019-06-07 13:04:29 +0530

    Fix UI issue and update plugin call

--- EnglishWebRoot/js/viewIndex.js
+++ EnglishWebRoot/js/viewIndex.js
@@ -1,18 +1,8 @@
 document.addEventListener("deviceready", function () {
     viewportFix();
-    window.plugins.locktask.startLockTask(function () {
-        if (typeof AndroidFullScreen !== "undefined") {
-            AndroidFullScreen.showSystemUI(function () {
-                cordova.plugins.autoStart.enable();
-                window.location.href = "welcome.html?firstLaunch";
-            }, function (err) {
-                alert(err);
-            });
-        } else {
-            // Full screen request either denied or plugin missing
-            window.location.href = "welcome.html?firstLaunch";
-        }
+    AndroidFullScreen.immersiveMode(function () {
+        window.location.href = "welcome.html?firstLaunch";
     }, function (err) {
-        alert(err);
-    }, "MyAdmin");
+                alert(err);
+    });
 });
